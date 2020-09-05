f59d417d023161ef0207eb4e9bf6b84db8c6d421
alexdlveliz <alexdlveliz@hotmail.com>
2020-09-04 18:59:54 -0600

    Agregado diseño experimental del login
    Co-authored-by alexdlveliz <alexdlveliz@hotmail.com>
    Co-authored-by NahomiTorres04 <naho_1998-2@outlook.es>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { LoginComponent } from "./login/components/login/login.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {
+      preloadingStrategy: PreloadAllModules
+    })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
