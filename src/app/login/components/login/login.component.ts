6ce3b838ab27788ee9154b5e20cf85e7a31adfc7
Héctor Tello <hectortllo@gmail.com>
2020-09-06 18:48:58 -0600

    1. Se añadió el modelo user. 2. Se añadió el servicio 'user' para las peticiones a la api concernientes al usuario. 3. Se agregaron formularios reactivos al usuario

--- src/app/login/components/login/login.component.ts
+++ src/app/login/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  formLogin: FormGroup;
+  formSignin: FormGroup;
+
   signin = true;
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.buildLoginForm();
+    this.buildSigninForm();
+  }
 
   ngOnInit(): void {
   }
 
+  // Función para iniciar sesión
+  login(event: Event): void {
+    event.preventDefault();
+    console.log(this.formLogin.value);
+  }
+
+  // Función para crear usuario
+  signIn(event: Event): void {
+    event.preventDefault();
+    console.log(this.formSignin.value);
+  }
+
+  private buildLoginForm(): void {
+    this.formLogin = this.formBuilder.group({
+      userLogin: ['', [Validators.required]],
+      emailLogin: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  private buildSigninForm(): void {
+    this.formSignin = this.formBuilder.group({
+      nameSignin: ['', [Validators.required]],
+      userSignin: ['', [Validators.required]],
+      emailSignin: ['', [Validators.required, Validators.email]],
+      passwordSignin: ['', [Validators.required]],
+      confirmationPassword: ['', [Validators.required]]
+    });
+  }
 }
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatFormFieldModule
   ],
   exports: [
-    MatToolbarModule
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule
   ]
 })
 export class MaterialModule { }
