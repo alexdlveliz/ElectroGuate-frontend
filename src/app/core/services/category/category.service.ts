7caec24d0206f255d8789c68c692370f55879271
Héctor Tello <hectortllo@gmail.com>
2020-11-03 23:41:51 -0600

    1. Se crearon los modelos necesarios para la orden y para el detalle de la orden. 2. Se crearon los servicios para la orden y para el detalle de las órdenes 3. Se corrigieron errores de hacia donde apuntaban los servicios

--- src/app/core/services/category/category.service.ts
+++ src/app/core/services/category/category.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Category } from './../../models/category.model';
-import { environment } from './../../../../environments/environment.prod';
+import { environment } from './../../../../environments/environment';
 import { Observable } from 'rxjs';
 import { map, retry } from 'rxjs/operators';
 
