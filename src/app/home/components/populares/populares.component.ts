d56e2ad8a86c884856549213da80d7fdd0d3a065
alexdlveliz <alexdlveliz@protonmail.com>
2020-11-17 00:40:48 -0600

    Avances en rediseño de la página principal
    Co-authored-by: alexdlveliz <alexdlveliz@hotmail.com>
    Co-authored-by: NahomiTorres04 <naho_1998-2@outlook.es>

--- src/app/home/components/populares/populares.component.ts
+++ src/app/home/components/populares/populares.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ProductService } from '@core/services/product/product.service';
+import { BrandService } from '@core/services/brand/brand.service';
+import { CategoryService } from '@core/services/category/category.service';
+import { CartService } from '@core/services/cart/cart.service';
+
+import { Product } from '@core/models/product.model';
+import { Brand } from '@core/models/brand.model';
+import { Category } from '@core/models/category.model';
 
 @Component({
   selector: 'app-populares',
@@ -7,9 +15,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PopularesComponent implements OnInit {
 
-  constructor() { }
+  brands: Brand[] = [];
+  categories: Category[] = [];
+  products: Product[] = [];
+
+  @Input() product: Product;
+  constructor(
+    private cartService: CartService,
+    private productService: ProductService,
+    private brandService: BrandService,
+    private categoryService: CategoryService
+  ) { }
 
   ngOnInit(): void {
+    this.fetchAllProducts();
+  }
+
+  addToCart(): void {
+    this.cartService.addCart(this.product);
+  }
+
+  async fetchAllProducts() : Promise<void> {
+    await this.fetchAllBrands();
+    await this.fetchAllCategories();
+    await this.productService.getAllProducts()
+    .subscribe(products => {
+      this.products = products;
+      console.log(this.products);
+    });
+  }
+
+  fetchAllBrands() : void {
+    this.brandService.getBrands()
+    .subscribe(brands => {
+      this.brands = brands;
+    })
+  }
+
+  fetchAllCategories(): void {
+    this.categoryService.getAllCategories()
+    .subscribe(categories => {
+      this.categories = categories;
+    })
   }
 
 }
